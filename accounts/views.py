d681fadee63bc5c59d7f5743962e7d70998c7bf2
현미 <akshwpsh@gmail.com>
2024-05-29 21:42:32 +0900

    데탑 업로드

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from NLMS.models import Profile, Group
+from accounts.models import Profile, Group
 
 
 # def login(request):
@@ -30,7 +30,7 @@ def login(request):
         # 인증 실패
         if user is None:
             context = {"error": "로그인 정보가 정확하지 않습니다."}
-            return render(request, "common/login/login.html", context)
+            return render(request, "accounts/login.html", context)
         # 인증 성공
         if user.profile.group.name == "professor":
             login(request, user)
@@ -44,19 +44,19 @@ def login(request):
         if request.user.is_authenticated:
             if request.user.profile.group.name == "professor":
                 # return professor.views.index(request)
-                return render(request, "common/login/login.html")
+                return render(request, "accounts/login.html")
 
             if request.user.profile.group.name == "student":
                 # return student.views.index(request)
-                return render(request, "common/login/login.html")
+                return render(request, "accounts/login.html")
 
         # 로그인 하지 않은 경우
         else:
-            return render(request, "common/login/login.html")
+            return render(request, "accounts/login.html")
 
 def logout(request):
     auth.logout(request)
-    return redirect("common:index")
+    return redirect("accounts:login")
 
 
 def register(request, roleName):
@@ -87,16 +87,16 @@ def register(request, roleName):
 
         user_auth = authenticate(request, username=username, password=password)
         login(request, user_auth)
-        return redirect("common:index")
+        return redirect("accounts:login")
 
     # 가입 GET
     else:
         if roleName == "professor":
-            return render(request, "common/register/professor_register.html")
+            return render(request, "accounts/professor_register.html")
         elif roleName == "student":
-            return render(request, "common/register/student_register.html")
+            return render(request, "accounts/student_register.html")
         else:
-            return redirect("common:index")
+            return redirect("accounts:login")
 
 
 def profile(request):
